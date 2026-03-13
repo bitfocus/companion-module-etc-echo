849a431143c6e6480c9f1a48ee97d9bcc2e993eb
Zachary Klosko <zklosko@hopecentral.com>
2026-03-12 21:18:39 -0400

    feat: move udp server to seperate file

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { UpdateActions } from './src/actions.js'
 import { UpdateFeedbacks } from './src/feedbacks.js'
 import { UpdateVariableDefinitions } from './src/variables.js'
 import { UpdatePresetDefinitions } from './src/presets.js'
-import dgram from 'node:dgram'
+import { createUDPServer } from './src/udp/server.js'
 
 export default class ModuleInstance extends InstanceBase {
 	constructor(internal) {
@@ -53,126 +53,7 @@ export default class ModuleInstance extends InstanceBase {
 
 		this.config = config
 
-		this.init_udp()
-	}
-
-	init_udp() {
-		this.updateStatus(InstanceStatus.Connecting)
-
-		if (this.config.host) {
-			this.udp = dgram.createSocket({ type: 'udp4', reuseAddr: true })
-			this.udp.bind({ port: this.config.serverport, address: this.config.selfIP }, () => {
-				this.updateStatus(InstanceStatus.Ok)
-				this.log('info', 'Listening for UDP packets on ' + this.config.serverport)
-			})
-
-			this.udp.on('error', (err) => {
-				this.updateStatus(InstanceStatus.ConnectionFailure, err.message)
-				this.log('error', 'Network error: ' + err.message)
-
-				this.udp.close()
-			})
-
-			// If we get data, thing should be good
-			this.udp.on('listening', () => {
-				this.updateStatus(InstanceStatus.Ok)
-			})
-
-			// Call when data gets received
-			this.udp.on('message', (msg, dInfo) => {
-				let dataResponse = msg.toString()
-				this.spaceRegexCheck = new RegExp(String.raw`.+?(?=: +${this.config.space})`)
-
-				if (this.spaceRegexCheck.test(dataResponse)) {
-					switch (true) {
-						case /E>pst act:/.test(dataResponse):
-							// Expected data response: E>pst act: <space> <active preset>
-							// If active preset == 0, no preset is applied
-							this.log('info', 'Preset data recieved')
-							let pstData = dataResponse.slice(11).split(', ')
-							this.EchoData.activePreset = pstData[1].replace(/(\r\n|\n|\r)/gm, '')
-
-							// Update variables in Companion
-							this.setVariableValues({
-								activePreset: this.EchoData.activePreset,
-							})
-
-							// Update feedbacks
-							this.checkFeedbacks('CheckInt', 'SpaceOff', 'ActivePreset')
-							break
-						case /E>space off:/.test(dataResponse):
-							// Expected data response: E>space off: <space> <truthy/falsy> (1 == truthy)
-							// Currently ignoring space data... assuming only using with one space
-							this.log('info', 'Space off data recieved')
-							let offData = dataResponse.slice(13).split(', ')
-							this.EchoData.spaceOff = offData[1].replace(/(\r\n|\n|\r)/gm, '') === '1' ? true : false
-
-							// Update variables in Companion
-							this.setVariableValues({
-								spaceOff: this.EchoData.spaceOff,
-							})
-
-							// Update feedbacks
-							this.checkFeedbacks('CheckInt', 'SpaceOff', 'ActivePreset')
-							break
-						case /E>seq act:/.test(dataResponse):
-							this.log('info', 'Sequence data recieved')
-							let seqData = dataResponse.slice(11).split(', ')
-							this.EchoData.activeSequence = seqData[1].replace(/(\r\n|\n|\r)/gm, '')
-
-							this.setVariableValues({
-								activeSequence: this.EchoData.activeSequence,
-							})
-							break
-						case /E>lok:/.test(dataResponse):
-							this.log('info', 'Sync data recieved')
-							break
-						case /E>zone int:/.test(dataResponse):
-							// Zone intensity data
-							this.log('info', 'Zone intensity data recieved')
-							let zoneData = dataResponse.split('E>zone int: ')
-							zoneData.shift() // First value in array should be blank
-							zoneData.forEach((zone) => {
-								let zRes = zone.split(', ')
-								this.EchoData.zonesInts[zRes[1] - 1] = zRes[2].replace(/(\r\n|\n|\r)/gm, '')
-							})
-
-							// Update variables in Companion
-							this.setVariableValues({
-								z1_int: this.EchoData.zonesInts[0],
-								z2_int: this.EchoData.zonesInts[1],
-								z3_int: this.EchoData.zonesInts[2],
-								z4_int: this.EchoData.zonesInts[3],
-								z5_int: this.EchoData.zonesInts[4],
-								z6_int: this.EchoData.zonesInts[5],
-								z7_int: this.EchoData.zonesInts[6],
-								z8_int: this.EchoData.zonesInts[7],
-								z9_int: this.EchoData.zonesInts[8],
-								z10_int: this.EchoData.zonesInts[9],
-								z11_int: this.EchoData.zonesInts[10],
-								z12_int: this.EchoData.zonesInts[11],
-								z13_int: this.EchoData.zonesInts[12],
-								z14_int: this.EchoData.zonesInts[13],
-								z15_int: this.EchoData.zonesInts[14],
-								z16_int: this.EchoData.zonesInts[15],
-							})
-
-							// Update feedbacks
-							this.checkFeedbacks('CheckInt', 'SpaceOff', 'ActivePreset')
-							break
-						default:
-							this.log('info', 'Unexpected UDP data received')
-							break
-					}
-				}
-			})
-
-			this.udp.on('status_change', (status, message) => {
-				this.updateStatus(status, message)
-			})
-		} else {
-			this.updateStatus(InstanceStatus.BadConfig)
-		}
+		createUDPServer(this)
 	}
 
 	// Return config fields for web config
