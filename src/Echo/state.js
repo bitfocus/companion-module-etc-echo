c2ea88b6778cda706c71376b4d3fd408dee3b6db
zklosko <31484611+zklosko@users.noreply.github.com>
2026-09-04 18:15:02 -0400

    rewrite class init

--- src/Echo/state.js
+++ src/Echo/state.js
@@ -10,24 +10,7 @@ export class EchoInstance extends EventEmitter {
 		// Map<space, { zones: Map<zone, value>, off: boolean, preset: value, sequence: value }>
 		this.spaces = new Map()
 
-		this.ZoneNames = []
-		this.SpaceNames = []
-		this.PresetNames = []
-
 		// Initialize class data
-		this.init()
-	}
-
-	// Initialize class
-	init() {
-		// Create values for dropdowns
-		for (let i = 1; i <= 16; i++) {
-			this.ZoneNames[i - 1] = { id: i, label: `Zone ${i}` }
-			this.SpaceNames[i - 1] = { id: i, label: `Space ${i}` }
-			this.PresetNames[i - 1] = { id: i, label: `Preset ${i}` }
-		}
-
-		// Init arrays with 0s
 		for (let space = 1; space <= 16; space++) {
 			const zones = new Map()
 			for (let zone = 1; zone <= 16; zone++) {
@@ -40,7 +23,6 @@ export class EchoInstance extends EventEmitter {
 				preset: 0,
 				sequence: 0,
 			})
-
 		}
 	}
 
